0d62ce3f7006bcc7d223ae823553c17d1e966bad
ASTOUND\o.taranov <o.taranov@astoundcommerce.com>
2021-03-17 16:05:24 +0100

    DEV: Webpack Init

---
+++ dev/js/pdp-base.js
@@ -0,0 +1,26 @@
+'use strict';
+
+export { pdp_page_data };
+
+const pdp_page_data = {
+    product: { 
+        title: 'Ultracolor Plus FA Grout',
+        price_piece: '$17.94',
+        price_box: '$12.96',
+        sku: '100512250',
+        variations: {
+            colors: {
+                color1: 'red',
+                color2: 'blue', 
+                color3: 'green',
+                color4: 'gray',
+                color5: 'black',
+                color6: 'white',
+            },
+            sizes: {
+                size1: '10lbs',
+                size2: '20lbs'
+            }
+        }
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import {pdp_page_data} from './../dev/js/pdp-base'; 
+import './test.css';
+import './test2.scss';
